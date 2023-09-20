aa6c6a931a3275e264997d8587ea3bdffc038ebf
Jordi Sanfeliu <sanfeliugarciajordi@gmail.com>
2023-09-20 11:10:26 +0200

    1.1

---
+++ app/src/main/java/jordi/sanfeliu/ejemplo_enviar_recibir_info/MainActivity.java
@@ -0,0 +1,64 @@
+package jordi.sanfeliu.ejemplo_enviar_recibir_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import jordi.sanfeliu.ejemplo_enviar_recibir_info.Modelos.Usuario;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private EditText txtPassword;
+    private Button btnDesencriptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inicializarVistas();
+        BtnDesencriptar();
+
+    }
+
+    private void BtnDesencriptar() {
+        btnDesencriptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (txtNombre.getText().toString().isEmpty() || txtPassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Rellene los campos", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    Bundle bundle = new Bundle();
+
+                    String nombre = txtNombre.getText().toString();
+                    String password = txtPassword.getText().toString();
+
+                    /*
+                    bundle.putString("NOMBRE",nombre);
+                    bundle.putString("PASSWORD",password);
+                     */
+
+                    Usuario u = new Usuario(nombre,password);
+                    bundle.putSerializable("OBJETO",u);
+
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void inicializarVistas() {
+        txtNombre = findViewById(R.id.txtNombreUsuarioMain);
+        txtPassword = findViewById(R.id.txtPasswordMain);
+        btnDesencriptar = findViewById(R.id.btnPasswordMain);
+    }
+}
---
+++ app/src/main/java/jordi/sanfeliu/ejemplo_enviar_recibir_info/MainActivity2.java
@@ -0,0 +1,33 @@
+package jordi.sanfeliu.ejemplo_enviar_recibir_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import jordi.sanfeliu.ejemplo_enviar_recibir_info.Modelos.Usuario;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        /*
+        if (bundle != null){
+            Toast.makeText(this, bundle.getString("NOMBRE") + " " + bundle.getString("PASSWORD"), Toast.LENGTH_SHORT).show();
+        }
+         */
+
+        Usuario u = (Usuario) bundle.getSerializable("OBJETO");
+
+        Toast.makeText(this, u.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+}
